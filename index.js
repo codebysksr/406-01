2b40f5ab3e947c52e1cc67c26cb35f493c33a618
sonali <sksr0056@gmail.com>
2023-01-25 16:31:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const countries=[];
+countries_data.map((e)=>{
+    countries.push(e.name.toLowerCase());
+})
+
+const p1=document.getElementsByClassName('p1');
+hello()
+
+function hello(){
+    p1[0].innerText=`Total Number of Countries : ${countries.length} `
+    const searchValue =document.getElementById("search").value;
+    
+    const lowsec=document.getElementById('lowerSec')
+    lowsec.innerHTML=""
+    if(searchValue!=""){
+
+   
+        let matchedCountories=[];
+        countries.map((e)=>{
+            if(e.startsWith(searchValue.toLowerCase())){
+                matchedCountories.push(e)
+                lowsec.innerHTML+=`<div class="card">${e}</div>`
+                p1[1].innerText=`There are  ${matchedCountories.length}  countries Starts with "${searchValue} "`
+                
+            }
+            
+            
+            
+        })
+        
+        
+    }
+    else{
+        lowsec.innerHTML+=`<h1>Enter Something</h1>`
+        p1[1].innerText=``
+        
+}
+}
+function hello1(){
+    p1[0].innerText=`Total Number of Countries : ${countries.length} `
+    const searchValue =document.getElementById("search").value;
+    if(searchValue!=""){
+
+        const lowsec=document.getElementById('lowerSec')
+        lowsec.innerHTML=""
+    
+        let matchedCountories=[];
+        countries.map((e)=>{
+            if(e.startsWith(searchValue.toLowerCase())){
+                matchedCountories.push(e)
+                lowsec.innerHTML+=`<div class="card">${e}</div>`
+                p1[1].innerText=`There are  ${matchedCountories.length}  countries Starts with "${searchValue} "`
+                
+            }
+            
+            
+        })
+
+        
+    }
+    else{
+        alert("Inter Something In SearchBox")
+    }
+}
+function data2(){
+    p1[0].innerText=`Total Number of Countries : ${countries.length} `
+    const searchValue =document.getElementById("search").value;
+    if(searchValue!=""){
+    const lowsec=document.getElementById('lowerSec')
+    lowsec.innerHTML=""
+  
+    let matchedCountories=[];
+    countries.map((el)=>{
+        if(el.includes(searchValue.toLowerCase())){
+            matchedCountories.push(el)
+            lowsec.innerHTML+=`<div class="card">${el}</div>`
+            p1[1].innerText=`There are  ${matchedCountories.length}  countries includes with ${searchValue}`
+
+            
+        }
+        
+        
+    })
+
+}
+else{
+        alert("Inter Something In SearchBox")
+    }
+
+}
